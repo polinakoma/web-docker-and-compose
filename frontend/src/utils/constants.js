519d3c3969d6a50680fd43ffec081c7b6c626e0c
Polina Komarova <polina-komarova-95@mail.ru>
2023-10-07 19:11:11 +0300

    backend url added

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = "http://api.giftservice.nomoredomainsrocks.ru";
+export const URL = "https://api.giftservice.nomoredomainsrocks.ru";
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
